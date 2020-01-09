e1c31e903a21dbf4c557c9c71b1222e75a35d81a
Dan Cashion <dangcashion@gmail.com>
2020-01-09 13:03:54 -0600

    brought login and registration buttons to the home page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -30,6 +30,7 @@ const Navigation = () => {
                     </>
                 }
                 </nav>
+                <Cube />
             </div>
             {/* <Cube isLoggedIn={true}  /> */}
         </div>
